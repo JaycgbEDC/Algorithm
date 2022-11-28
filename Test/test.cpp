a1e9589cda7ec9b3cc436e018f708a17dd1769b5
Kribe <2289109260@qq.com>
2022-11-28 18:48:21 +0800

    11.28

--- Test/test.cpp
+++ Test/test.cpp
@@ -3,78 +3,75 @@
 
 using namespace std;
 
-struct BiTNode
+
+typedef char ElemType;
+typedef struct BiTNode
 {
-    int data;
-    BiTNode *lchild, *rchild;
-
-    BiTNode(int data)
-    {
-        this->data = data;
-        this->lchild = this->rchild = nullptr;
-    }
-};
+	ElemType data; 
+	struct BiTNode* lchild;
+	struct BiTNode* rchild;
+}BiTNode, *BiTree;	
 
 
-BiTNode* Latest_ComnAncestor(BiTNode *T, BiTNode *q1, BiTNode *q2)
+void CreateBiTree(FILE *fp, BiTree &T)
 {
-    //寻找q1和q2的最近公共祖先（假设q1在q2左边）
-    BiTNode *s[20], *s1[20];
-    int top = -1, top1 = -1;
-    BiTNode *p = T, *last;
-    while(p || -1 != top) {
-        if(p) { //一路向左
-            s[++top] = p;
-            p = p->lchild;
+	char ch;
+
+	fscanf(fp, "%c", &ch);
+	
+	if(ch == '^')
+		T = NULL;
+	else
+	{
+		T = (BiTree)malloc(sizeof(BiTNode));
+		if(!T)
+			exit(EOVERFLOW);
+		T->data = ch;
+		CreateBiTree(fp, T->lchild);
+		CreateBiTree(fp, T->rchild);
+	}
+    return;
+}
+
+
+int Max_Width(BiTNode *T)
+{
+    //求非空二叉树的最大宽度--->层次遍历
+    BiTNode *Q[20];
+    int front = -1, rear = -1;
+    int curl = 0, curw = 0; //curl表示当前层最后一个结点，curw表示当前层宽度
+    int max = 0; //最大二叉树宽
+    BiTNode *p = T;
+    Q[++rear] = p;
+    while(front != rear) {
+        p = Q[++front];
+        curw++;
+        if(p->lchild) {
+            Q[++rear] = p->lchild;
         }
-        else {
-            p = s[top];
-            if (p && p == q1) //找到q1，将s中的祖先复制到辅助栈s1中
-                for (int i = 0; i <= top; i++)
-                    s1[++top1] = s[i];
-            if(p->rchild && p->rchild != last)
-                p = p->rchild;
-            else {
-                top--;
-                if (p && p == q2){ //找到q2
-                    int i;
-                    for (i = 0; i <= top && i <= top1 && s[i] == s1[i]; i++);
-                    if (i > 0)
-                        return s[i - 1];
-                }
-                last = p;
-                p = NULL;
-            }
+        if(p->rchild) {
+            Q[++rear] = p->rchild;
+        }
+        if(front == curl) { //当前出栈结点为当层最后一个结点
+            curl = rear;
+            if(curw > max)
+                max = curw;
+            curw = 0;
         }
     }
-    return NULL;
+    return max;
 }
 
 int main()
 {
-    /* 构造下面的树
-          1
-        /   \
-       /     \
-      2       3
-       \     / \
-        4   5   6
-           / \
-          7   8
-    */
+    BiTree T = NULL;
+    FILE *fp;
+    fp = fopen("TestData.txt", "r");
+    CreateBiTree(fp, T);
+    fclose(fp);
 
-    BiTNode *root = new BiTNode(1);
-    root->lchild = new BiTNode(2);
-    root->rchild = new BiTNode(3);
-    root->lchild->rchild = new BiTNode(4);
-    root->rchild->lchild = new BiTNode(5);
-    root->rchild->rchild = new BiTNode(6);
-    root->rchild->lchild->lchild = new BiTNode(7);
-    root->rchild->lchild->rchild = new BiTNode(8);
-
-    BiTNode *p = Latest_ComnAncestor(root, root->rchild->lchild->lchild, root->rchild->lchild->rchild);
-    if(p)
-        cout << p->data << endl;
+    int a = Max_Width(T);
+    cout << a << endl;
     system("pause");
     return 0;
 }
