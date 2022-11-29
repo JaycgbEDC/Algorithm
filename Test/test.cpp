ff0996034ab2215bc6eb968aaa20cfac4a9e0e2c
Kribe <2289109260@qq.com>
2022-11-29 14:40:11 +0800

    1

--- Test/test.cpp
+++ Test/test.cpp
@@ -36,7 +36,7 @@ void CreateBiTree(FILE *fp, BiTree &T)
 
 void Pre_ToPost(ElemType pre[], int l1, int h1, ElemType post[], int l2, int h2)
 {
-    //满二叉树，已知先序求后序
+    //满二叉树，已知先序求后序(满二叉树意味着左右子树的结点数必相等)
     int half = (h1 - l1) / 2;
     if(l1 <= h1) {
         post[h2] = pre[l1];
