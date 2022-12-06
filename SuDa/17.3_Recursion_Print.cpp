c79ed20f8305524c04ea10725cb693516b53e43e
Kribe <2289109260@qq.com>
2022-12-06 15:47:48 +0800

    输出该序列最小的100个数，要求采用递归思想

--- SuDa/17.3_Recursion_Print.cpp
+++ SuDa/17.3_Recursion_Print.cpp
@@ -1,19 +1,20 @@
-#include <iostream>
-#include <stdlib.h>
-#include "time.h"
-
-using namespace std;
+/*有这样的序列：
+    1.整数1在序列中
+    2.若x在序列中，则2x、3x、5x也在序列中
+    3.除此之外没有别的数
+    要求：输出该序列最小的100个数，要求采用递归思想
+*/
 
 bool IsMeetNum(int n) {
     //判断数n是否在序列中
     if(1 == n)
         return true;
     else if(0 == n % 2)
-        return IsMeetNum(n / 2);
+        return IsMeetNum(n % 2);
     else if(0 == n % 3)
-        return IsMeetNum(n / 3);
+        return IsMeetNum(n % 3);
     else if(0 == n % 5)
-        return IsMeetNum(n / 5);
+        return IsMeetNum(n % 5);
     else
         return false;
 }
@@ -25,14 +26,4 @@ void PrintMin100() {
             i++;//只有在序列中才输出下一个数
         }
     }
-}
-
-
-int main() {
-
-    PrintMin100();
-    cout << endl;
-
-    system("pause");
-    return 0;
 }
